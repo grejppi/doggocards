35c5e23d1836b4f551cb14fe9117538605b535ce
Henna Haahti <grejppi@users.noreply.github.com>
2020-04-07 14:23:27 +0300

    Footer

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -5,9 +5,13 @@ import ScoreCard from './scorecard/ScoreCard';
 
 function App() {
   return (
-    <div className="App fixed top-0 w-full">
+    <div className="App fixed top-0 w-full flex flex-col h-full">
       <Board />
       <ScoreCard />
+      <div className="flex-grow"></div>
+      <footer className="p-2">
+        Photos provided by <a href="https://dog.ceo/dog-api/" className="text-blue-600">dog.ceo Dog API</a>
+      </footer>
     </div>
   );
 }
